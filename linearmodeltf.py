a8c07f623b10198d96639839460945d311933cea
srinivas <rsri1011@gmail.com>
2017-06-13 04:29:39 +0530

    updated linear model

--- linearmodeltf.py
+++ linearmodeltf.py
@@ -38,23 +38,16 @@ class LinearModel:
 			sess.run(tf.initialize_all_variables())
 			for epoch in range(self.epoch):
 				epoch_loss = 0
-				for i in range(self.mini_batch_size):
-					_, c = sess.run([optimizer, cost], feed_dict= {self.x:train_X, self.y:train_Y})
-					epoch_loss += c
-					print 'epoch loss' 
-			
-						
-	#Test the model:
-	def test_model(self, test_X, test_Y):
-		with tf.Session() as sess:
-			sess.run(tf.initialize_all_variables())
-			predicted_val = self.feed_forward(self.x)
+				_, c = sess.run([optimizer, cost], feed_dict= {self.x:train_X, self.y:train_Y})
+				epoch_loss += c
+				print 'epoch loss' 
+				predicted_val = self.feed_forward(self.x)
+
+			print('Testing the model')
 			correct_prediction = tf.equal(tf.argmax(predicted_val,1), tf.argmax(self.y,1))
 			accuracy = tf.reduce_mean(tf.cast(correct_prediction, 'float'))
 			print('acc', accuracy.eval({self.x: test_X, self.y: test_Y}))		
-		 # Graphic display
-		
-		
+			
 
 if __name__ == '__main__':
 	
@@ -64,13 +57,17 @@ if __name__ == '__main__':
 	train_Y = np.matrix([1.7,2.76,2.09,3.19,1.694,1.573,3.366,2.596,2.53,1.221,2.827,3.465,1.65,2.904,2.42,2.94,1.3], dtype = np.float32)
 	train_Y = np.transpose(train_Y)
 	
+	#Test data for the model
+	test_X = np.matrix([6.83, 4.668, 8.9, 7.91, 5.7, 8.7, 3.1, 2.1, 6.83, 4.668, 8.9, 7.91, 5.7, 8.7, 3.1, 2.1, 1.1], dtype = np.float32)
+	test_X = np.transpose(test_X)
+	test_Y = np.matrix([1.84, 2.273, 3.2, 2.831, 2.92, 3.24, 1.35, 1.03,6.83, 4.668, 8.9, 7.91, 5.7, 8.7, 3.1, 2.1, 1.1], dtype = np.float32)
+	test_Y = np.transpose(test_Y)
 	
 	#Model parameters
 	n_samples = 17
 	batch_size = 100
-	epochs = 1
+	epochs = 100
 	learning_rate = 0.001
-	mini_batch_size = 1
 	
 	
 	lin_model_obj = LinearModel()
@@ -82,17 +79,8 @@ if __name__ == '__main__':
 	
 	#Train the network
 	print 'Training the model'
-	lin_model_obj.train_network(train_X, train_Y)
-	
-	#Test data for the model
-	test_X = np.matrix([6.83, 4.668, 8.9, 7.91, 5.7, 8.7, 3.1, 2.1, 6.83, 4.668, 8.9, 7.91, 5.7, 8.7, 3.1, 2.1, 1.1], dtype = np.float32)
-	test_X = np.transpose(test_X)
-	test_Y = np.matrix([1.84, 2.273, 3.2, 2.831, 2.92, 3.24, 1.35, 1.03,6.83, 4.668, 8.9, 7.91, 5.7, 8.7, 3.1, 2.1, 1.1], dtype = np.float32)
-	test_Y = np.transpose(test_Y)
-	
-	print 'Testing the model'
-	lin_model_obj.test_model(test_X, test_Y)
-	
+	lin_model_obj.train_network(train_X, train_Y, test_X, test_Y)
+
 	#Done 
 	print 'Done'
 	
